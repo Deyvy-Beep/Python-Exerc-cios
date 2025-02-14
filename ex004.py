40f409b8ae506008ee2c5b1689fc85633b44a702
Deyvy-beep <deyvygomes663@gmail.com>
2025-02-14 09:43:00 -0300

    ex de tipos primitivos

--- ex004.py
+++ ex004.py
@@ -1,3 +1,12 @@
 algo = input('Digite algo: ')
 
-print(algo.isnumeric)
+print('\nO tipo primitivo desse valor é ', type(algo))
+
+print('\nSo tem espaço?', algo.isspace())
+print('\nÉ número?', algo.isnumeric())
+print('\nÉ alfabético?', algo.isalpha())
+print('\nÉ alfanumérico?', algo.isalnum())
+print('\nEstá em maiúsculo?', algo.isupper())
+print('\nEstá em minúsculo?', algo.islower())
+print('\nEstá capitalizada?', algo.istitle())
+print('\n')
