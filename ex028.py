305907699d9c5ee7e6b9599b4ce198ffb1580427
Deyvy-beep <deyvygomes663@gmail.com>
2025-02-20 14:20:38 -0300

    Exercicios de Condições

---
+++ ex028.py
@@ -0,0 +1,25 @@
+import random
+from time import sleep
+
+print('=-='*20)
+print('Vou pensar em número entre 0 e 5, tente adivinhar!')
+print('=-='*20)
+
+print('pensado...')
+sleep(1)
+num = random.randint(0, 5)
+
+chute = int(input('Qual número eu pensei? '))
+
+if chute > 5 or chute < 0:
+    print('\nEu pensei em um número entre 0 e 5. Tente de novo.\n')
+    breakpoint
+else:
+    if chute == num:
+        print('=-='*20)
+        print('\n Você acertou. PARABÉNS!! \n')
+        print('=-='*20)
+    else:
+        print('=-='*20)
+        print('\n O número foi {}. Eu ganhei!! \n'.format(num))
+        print('=-='*20)
